ff19e67418c553a31be7697a5508abb62c9b120f
Frank Wolff Hannemann <frnkwh@proton.me>
2025-09-10 21:47:54 -0300

    Arrumando a transposicao colunar

--- t1/transposition.py
+++ t1/transposition.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-text = "eugostomuitodagatinha"
-key = "xandao"
+text = "TRANSPOSITIONCIPHERSSCRAMBLELETTERSLIKEPUZZLEPIECESTOCREATEANINDECIPHERABLEARRANGEMENT"
+key = "JANEAUSTEN"
+#text = "123456123456123456123456123456123456"
+#key = "zebras"
 key_len = len(key)
 
 def string_to_matrix(text, key_len, pad=" "):
@@ -13,15 +15,6 @@ def string_to_matrix(text, key_len, pad=" "):
     arr = np.array(list(text)).reshape(-1, key_len)
     return arr
 
-matrix = string_to_matrix(text, key_len)
-
-print("Matriz Original")
-print(matrix)
-
-print("Matriz Transposta")
-print(matrix.T)
-
-
 def alphabetical_order(key):
     sorted_key = sorted([(char, i) for i, char in enumerate(key)])
     print(sorted_key)
@@ -31,8 +24,32 @@ def alphabetical_order(key):
         order[i] = rank
     return order
 
-new_order = alphabetical_order(key)
-print(f"new_order: {new_order}")
-reordered = matrix[:, new_order]
-print("Matriz Reordenada")
-print(reordered)
+def columnar_transposition(matrix, key, key_len):
+    new_order = alphabetical_order(key)
+    zipped = np.column_stack((np.arange(key_len), new_order))
+    sorted_zipped = zipped[zipped[:, 1].argsort()]
+    indices = sorted_zipped[:, 0]
+    transposed = matrix[:, indices]
+
+    return transposed
+
+#nao funciona ainda, tem que trocar essa funcao inteira
+def transpose_fixed_cols(matrix, key_len):
+    flat = matrix.flatten()
+    result = [flat[i:i + key_len] for i in range(0, len(flat), key_len)]
+    return result
+
+matrix = string_to_matrix(text, key_len)
+print("Matriz Original")
+print(matrix)
+
+transposed = columnar_transposition(matrix, key, key_len)
+print("Transposto Colunarmente")
+print(transposed)
+
+transposed = transpose_fixed_cols(transposed, len("AEROPLANES"))
+print(transposed)
+double = columnar_transposition(transposed, "AEROPLANES", len("AEROPLANES"))
+print("Transposto Duplamente Colunarmente")
+print(double)
+
